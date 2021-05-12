b7650dd9cc5f3c3ecb66aaabba61b6df7df8fd22
Grawling <grawling08@gmail.com>
2021-05-12 10:17:48 +0800

    Login Added with Page Restriction, Removed ID field in Create new Users

--- WebApp1/Controllers/HomeController.cs
+++ WebApp1/Controllers/HomeController.cs
@@ -1,16 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using WebApp1.Models;
 
 namespace WebApp1.Controllers
 {
     public class HomeController : Controller
     {
+        private webapp_dbEntities db = new webapp_dbEntities();
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string username, string password)
+        {
+            if (ModelState.IsValid)
+            {
+                var f_password = GetMD5(password);
+                var data = db.users.Where(s => s.username.Equals(username) && s.password.Equals(password)).ToList();
+                if (data.Count() > 0)
+                {
+                    //add session
+                    Session["FullName"] = data.FirstOrDefault().fname + " " + data.FirstOrDefault().lname;
+                    Session["idUser"] = data.FirstOrDefault().id;
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.error = "Login failed";
+                    return RedirectToAction("Login");
+                }
+            }
+            return View();
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();//remove session
+            return RedirectToAction("Login");
         }
 
         public ActionResult About()
@@ -25,6 +64,22 @@ namespace WebApp1.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        //create a string MD5
+        public static string GetMD5(string str)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] fromData = Encoding.UTF8.GetBytes(str);
+            byte[] targetData = md5.ComputeHash(fromData);
+            string byte2String = null;
+
+            for (int i = 0; i < targetData.Length; i++)
+            {
+                byte2String += targetData[i].ToString("x2");
+
+            }
+            return byte2String;
         }
     }
 }
