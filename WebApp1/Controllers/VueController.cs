3b42084f25df2c670be4ed04c9f54fb55c33b6ed
Grawling <grawling08@gmail.com>
2021-05-20 11:33:34 +0800

    Fixed: vue module => uploading images, hashing password, reloading data in frontend after save.

--- WebApp1/Controllers/VueController.cs
+++ WebApp1/Controllers/VueController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
@@ -33,7 +34,7 @@ namespace WebApp1.Controllers
         public JsonResult SaveUsers(user json)
         {
             //var _users = id;
-
+            json.password = HomeController.GetMD5(json.password);
             var _users = db.users.Add(json);
             db.SaveChanges();
 
@@ -43,11 +44,41 @@ namespace WebApp1.Controllers
         [HttpPost]
         public JsonResult EditUser(user json)
         {
-
+            json.password = HomeController.GetMD5(json.password);
             var _users = db.Entry(json).State = EntityState.Modified;
             db.SaveChanges();
-            return Json(_users, JsonRequestBehavior.AllowGet);
 
+            return Json(_users, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult DeleteUser(int id)
+        {
+            user user = db.users.Find(id);
+            var _users = db.users.Remove(user);
+            db.SaveChanges();
+
+            return Json(_users, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult UploadImg(HttpPostedFileBase file, int id)
+        {
+            HttpContext context = System.Web.HttpContext.Current;
+            HttpPostedFile postedFile = context.Request.Files.Count > 0 ? context.Request.Files.Get(0) : null;
+
+            string filename = Path.GetFileName(postedFile.FileName);
+            string _filename = DateTime.Now.ToString("yymmssfff") + filename.Replace(" ", String.Empty);
+            string extension = Path.GetExtension(postedFile.FileName);
+            string path = Path.Combine(Server.MapPath("~/Images/"), _filename);
+
+            user user = db.users.Find(id);
+            user.pic = "~/Images/" + _filename.ToString();
+            var _users = db.Entry(user).State = EntityState.Modified;
+            db.SaveChanges();
+            postedFile.SaveAs(path);
+
+            return Json(_users, JsonRequestBehavior.AllowGet);
         }
 
         
